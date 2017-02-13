c74fa900e73567339bd4157d6f1e4efff94c274e
Martin Danielsson <martin@danielsson.de>
2017-02-13 21:18:44 +0100

    Haufe-Lexware/wicked.haufe.io#53 Special case running as root

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,14 +2,33 @@
 
 set -e
 
+# On Linux, you will potentially run into permission problems if
+# you do not pass in LOCAL_UID and LOCAL_GID; in case you pass in
+# these two variables, the kickstarter will run as a user having
+# these numeric IDs for user id and group id.
+startedNode=0
 if [ ! -z "${LOCAL_UID}" ] && [ ! -z "${LOCAL_GID}" ]; then
-    echo "Creating local user in docker container..."
-    groupadd -r usergroup --gid=${LOCAL_GID}
-    useradd -r -g usergroup --uid=${LOCAL_UID} user
+    if [ ! "${LOCAL_GID}" = "0" ]; then
+        echo "Adding group with GID ${LOCAL_GID}..."
+        groupadd -r usergroup --gid=${LOCAL_GID}
+    fi
+    # Running as root?
+    if [ ! "${LOCAL_UID}" = "0" ]; then
+        echo "Not running as root..."
+        if [ ! "${LOCAL_GID}" = "0" ]; then
+            echo "Creating user with special group and UID ${LOCAL_UID}..."
+            useradd -r -g usergroup --uid=${LOCAL_UID} user
+        else
+            echo "Creating user with group 'root' and UID ${LOCAL_UID}..."
+            useradd -r -g root --uid=${LOCAL_UID} user
+        fi
+        echo "Running with UID ${LOCAL_UID} and GID ${LOCAL_GID}."
+        startedNode=1
+        gosu user node bin/kickstart "$@"
+    fi
+fi
 
-    echo "Running with UID ${LOCAL_UID} and GID ${LOCAL_GID}."
-    # chown -R user:usergroup /var/portal-api
-    gosu user node bin/kickstart "$@"
-else
+if [ startedNode=0 ]; then
+    echo "Running kickstarter as root..."
     node bin/kickstart "$@"
 fi
