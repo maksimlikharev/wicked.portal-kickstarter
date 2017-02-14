c4c58405b31de84cbbe0804caf5d35958f46bf71
Martin Danielsson <martin@danielsson.de>
2017-02-14 12:58:42 +0100

    More tweaking to make kickstarter run well on Linux

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -10,21 +10,26 @@ startedNode=0
 if [ ! -z "${LOCAL_UID}" ] && [ ! -z "${LOCAL_GID}" ]; then
     if [ ! "${LOCAL_GID}" = "0" ]; then
         echo "Adding group with GID ${LOCAL_GID}..."
-        groupadd -r usergroup --gid=${LOCAL_GID}
+        if groupadd -r usergroup --gid=${LOCAL_GID}; then
+            echo "Succeeded."
+        else
+            echo "Did not succeed, assuming group already present."
+        fi
     fi
     # Running as root?
     if [ ! "${LOCAL_UID}" = "0" ]; then
-        echo "Not running as root..."
-        if [ ! "${LOCAL_GID}" = "0" ]; then
-            echo "Creating user with special group and UID ${LOCAL_UID}..."
-            useradd -r -g usergroup --uid=${LOCAL_UID} user
+        username="localuser"
+        if [ ! "${LOCAL_UID}" = "1000" ]; then
+            echo "Not running as root or user node..."
+            echo "Creating user..."
+            useradd -r -g ${LOCAL_GID} --uid=${LOCAL_UID} localuser
         else
-            echo "Creating user with group 'root' and UID ${LOCAL_UID}..."
-            useradd -r -g root --uid=${LOCAL_UID} user
+            echo "Using predefined user 'node' (UID 1000)."
+            localuser="node"
         fi
         echo "Running with UID ${LOCAL_UID} and GID ${LOCAL_GID}."
         startedNode=1
-        gosu user node bin/kickstart "$@"
+        gosu $localuser node bin/kickstart "$@"
     fi
 fi
 
